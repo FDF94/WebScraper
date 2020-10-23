c7782c99f8907bdafe00b097b4270f26191ee171
FDF94 <fdelfedele@gmail.com>
2020-10-23 19:24:19 -0300

    Added a simple HTML analyzer to count elements and tags

---
+++ HTMLAnalyzer.py
@@ -0,0 +1,18 @@
+from html.parser import HTMLParser
+from collections import Counter
+
+
+class HTMLAnalyzer(HTMLParser):
+    _open_tags = []
+
+    def handle_starttag(self, tag, attrs):
+        self._open_tags.append(tag)
+
+    def handle_endtag(self, tag):
+        pass
+
+    def get_tags_count(self):
+        return Counter(self._open_tags)
+
+    def get_elements_count(self):
+        return len(self._open_tags)
--- WebScraper.py
+++ WebScraper.py
@@ -1,12 +1,17 @@
 import argparse
 from urllib import request
+from HTMLAnalyzer import HTMLAnalyzer
 
 
 def ScrapeWeb(url: str):
     with request.urlopen(url) as page:
         content = page.read().decode("utf8")
 
-    print(content)
+    parser = HTMLAnalyzer()
+    parser.feed(content)
+
+    print(parser.get_tags_count())
+    print(parser.get_elements_count())
 
 
 parser = argparse.ArgumentParser()
