8e968877485b5d54a11df00c220b79b41c38ccdd
FDF94 <fdelfedele@gmail.com>
2020-10-24 14:39:15 -0300

    Added unit test for get_html_content

---
+++ test_get_html_content.py
@@ -0,0 +1,44 @@
+import unittest
+from get_html_content import get_html_content
+import logging
+
+
+logging.disable(logging.CRITICAL)
+
+
+class TestGetHtmlContent(unittest.TestCase):
+
+    def test_get_content(self):
+        # Arrange
+        url = "https://www.endava.com/"
+
+        # Act
+        content = get_html_content(url)
+
+        # Assert
+        self.assertIsNotNone(content)
+
+    def test_get_content_fail_scenarios(self):
+        with self.subTest("Invalid URL type"):
+            # Arrange
+            url = "www.endava.com/"
+
+            # Act
+            content = get_html_content(url)
+
+            # Assert
+            self.assertIsNone(content)
+        with self.subTest("Unexistant URL"):
+            # Arrange
+            # As I'm writing this, it doesn't exist
+            url = "http://www.CompletelyFakeURL.com/"
+
+            # Act
+            content = get_html_content(url)
+
+            # Assert
+            self.assertIsNone(content)
+
+
+if __name__ == '__main__':
+    unittest.main()
