d56a3ab3c55eac7b87f010d5fb9b34b22fcfcc46
FDF94 <fdelfedele@gmail.com>
2020-10-25 16:59:22 -0300

    Added control to ignore elements inside other designated elements

--- HTMLAnalyzer.py
+++ HTMLAnalyzer.py
@@ -7,12 +7,29 @@ class HTMLAnalyzer(HTMLParser):
     def __init__(self):
         super().__init__()
         self._open_tags = []
+        self._close_tags = []
+        self._ignore_tags = False
+        self._excluded_blocks = ["pre"]
+        self._current_excluding = None
 
     def handle_starttag(self, tag, attrs):
-        self._open_tags.append(tag)
+        if not self._ignore_tags:
+
+            self._open_tags.append(tag)
+
+            if tag in self._excluded_blocks:
+                self._current_excluding = tag
+                self._ignore_tags = True
 
     def handle_endtag(self, tag):
-        pass
+        if not self._ignore_tags:
+            self._close_tags.append(tag)
+        # Checking self._ignore_tags again is not really necessary
+        # It is left for readability
+        elif self._ignore_tags and self._current_excluding == tag:
+            self._ignore_tags = False
+            self._current_excluding = None
+            self._close_tags.append(tag)
 
     def get_tags_count(self):
         return Counter(self._open_tags)
--- test_HTMLAnalyzer.py
+++ test_HTMLAnalyzer.py
@@ -35,6 +35,23 @@ class TestGetHtmlContent(unittest.TestCase):
         # Assert
         self.assertEqual(parser.get_tags_count(), tags_result)
 
+    def test_ignore_elements(self):
+        # Arrange
+        parser = HTMLAnalyzer()
+        sample_html = ("""<html>
+                       <head>
+                       <title>Test</title>
+                       </head>
+                       <body><h1>Parse me!</h1></body>
+                       <pre><h2>This h2 should be ignored<h2/><pre/>
+                       </html>""")
+        tags_result = Counter(["html", "head", "title", "body", "h1", "pre"])
+
+        # Act
+        parser.feed(sample_html)
+
+        # Assert
+        self.assertEqual(parser.get_tags_count(), tags_result)
 
 if __name__ == '__main__':
     unittest.main()
