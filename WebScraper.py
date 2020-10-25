cbb5e54a1abd8490e63ea44a3dd46af266b97c83
FDF94 <fdelfedele@gmail.com>
2020-10-25 15:29:46 -0300

    Added option to write results to a file

--- WebScraper.py
+++ WebScraper.py
@@ -7,20 +7,38 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
-def ScrapeWeb(url: str):
+def ScrapeWeb(url: str, filename: str = None):
 
     content = get_html_content(url)
 
-    if content:
-        parser = HTMLAnalyzer()
-        parser.feed(content)
+    if not content:
+        return None
 
-        logging.info(parser.get_tags_count())
-        logging.info(parser.get_elements_count())
+    parser = HTMLAnalyzer()
+    parser.feed(content)
+
+    tags = parser.get_tags_count().most_common(5)
+    elements_number = parser.get_elements_count()
+
+    if filename:
+        with open(filename, "w") as f:
+            f.write("There are {} HTML elements\n".format(elements_number))
+            for tag, n in tags:
+                f.write("Tag {} appears {} times\n".format(tag, n))
+        return None
+    else:
+        return elements_number, tags
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-u", "--url", default="http://www.python.org",
-                    help="URL of web page to parse", type=str)
-args = parser.parse_args()
-ScrapeWeb(args.url)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--url", default="http://www.python.org",
+                        help="URL of web page to parse.", type=str)
+    parser.add_argument("-f", "--filename", default=None,
+                        help="Filename in which to write results. Optional.",
+                        type=str)
+    args = parser.parse_args()
+    if args.filename is None:
+        print(ScrapeWeb(args.url))
+    else:
+        ScrapeWeb(args.url, args.filename)
